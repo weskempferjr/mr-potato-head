7725055ff3618de72599441ead481e63035e15af
Wes Kempfer <wkempferjr@tnotw.com>
2019-11-07 18:22:08 -0800

    preserve publish status for updated listing

--- includes/class-mph-post-expiration-manager.php
+++ includes/class-mph-post-expiration-manager.php
@@ -9,9 +9,7 @@
 class MPH_Post_Expiration_Manager {
 
 	public function expiration_content_filter( $template_path ) {
-
-		// str_replace('.tpl.php', '', $template_path ) . '-custom.tpl.php'
-
+		
 		$filtered_template_path  = dirname( plugin_dir_path(__FILE__ ) ) . '/alsp/'  . str_replace('.tpl.php', '', $template_path ) . '-custom.tpl.php';
 
 		if ( is_file( $filtered_template_path )) {
@@ -49,6 +47,33 @@ class MPH_Post_Expiration_Manager {
 		}
 	}
 
+	/*
+	 * This function is called via the pre_post_update function. If a post is an alsp_listing,
+	 * post_status is pending and if the expiration date is less than 1 week, set status back to published.
+	 *
+	 */
+	public function preserve_published_status_after_update( $post_id, $after_post, $before_post ) {
+
+		$post_type = $before_post->post_type;
+		$before_status = $before_post->post_status ;
+		$after_status = $after_post->post_status ;
+
+		if ( $post_type == 'alsp_listing' && $before_status == 'publish'  && $after_status == 'pending') {
+
+			$expire = intval( get_post_meta( $post_id, '_expiration_date', true) );
+			$now = time();
+
+			$updated_post = $after_post ;
+			$updated_post->post_status = 'publish';
+
+			if (   $now < $expire ) {
+				wp_update_post( $updated_post );
+			}
+
+		}
+
+	}
+
 	public function add_filters( $loader ) {
 		// $loader->add_filter('the_content', $this ,'expiration_content_filter');
 		// $loader->add_filter('alsp_listing_display_template', $this ,'expiration_content_filter');
@@ -63,6 +88,7 @@ class MPH_Post_Expiration_Manager {
 		// $loader->add_action('alsp_listing_title_html', $this, 'add_expiration_pre_content');
 
 		$loader->add_action('difp_action_message_after_send', $this, 'resetExpirationOnBid', 10, 3);
+		$loader->add_action('post_updated', $this, 'preserve_published_status_after_update', 10, 3);
 	}
 
 
